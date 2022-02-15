6523bddd8dd9eaf2fd7e30b856e4570dd9450567
jpsrm <97506106+jpsrm@users.noreply.github.com>
2022-02-15 17:05:53 +0530

    Add files via upload

---
+++ cft1.tf
@@ -0,0 +1,21 @@
+
+provider "aws" {
+region = "ap-southeast-1"
+alias =  "Singapore"
+}
+variable "acc_no" {
+  type = string
+}
+
+variable "access_key" {
+  type = string
+}
+
+variable "secret_access_key" {
+  type = string
+}
+resource "aws_cloudformation_stack" "project" {
+  name = "project-stack"
+  capabilities     = ["CAPABILITY_NAMED_IAM"]
+  template_url = "https://s3-projectnew1.s3.ap-south-1.amazonaws.com/project3.json"
+}
