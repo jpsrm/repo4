2b7726ff9b7f938b9c56aeed3f1e5e74c5faf360
jpsrm <97506106+jpsrm@users.noreply.github.com>
2022-08-17 16:31:07 +0530

    Create task.tf

---
+++ task.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "us-east-1"
+
+}
+variable "acc_no" {
+ type = string
+}
+
+variable "access_key" {
+  type = string
+
+variable "secret_access_key" {
+  type = string
+}
+resource "aws_iam_user" "shree"{
+    name = "shree"
+	}
+resource "aws_cloudformation_stack" "project" {
+  name = "project-stack"
+  capabilities     = ["CAPABILITY_NAMED_IAM"]
+   template_body = <<STACK
+   {
+        "Resources": {
+                "Policy": {
+                        "Type": "AWS::IAM::ManagedPolicy",
+                        "Properties": {
+                                "ManagedPolicyName": "S3policynew2",
+                                "PolicyDocument": {
+                                        "Version": "2012-10-17",
+                                        "Statement": [{
+                                                "Effect": "Allow",
+                                                "Action": [
+                                                        "s3:Get*",
+                                                        "s3:List*"
+                                                ],
+                                                "Resource": "*"
+                                        }]
+                                },
+								"Users": ["shree"]
+                        }
+                }
+        }
+}
+STACK
+}
